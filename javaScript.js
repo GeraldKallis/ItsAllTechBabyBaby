c0cfe94396ac4546137b5c7e989f8ffd27cabdc1
GeraldKallis <gerald.kallis@gmail.com>
2022-06-08 16:34:22 +0200

    JavaScript Project

---
+++ javaScript.js
@@ -0,0 +1,166 @@
+(function(){
+  // Functions
+  function buildQuiz(){
+   
+    const output = [];
+
+   
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        
+        const answers = [];
+
+       
+        for(letter in currentQuestion.answers){
+
+        
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+   
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+   
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+   
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+   
+    let numCorrect = 0;
+
+  
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+
+      if(userAnswer === currentQuestion.correctAnswer){
+       
+        numCorrect++;
+
+      
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+    
+      else{
+       
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+   
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Questions to be answered 
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "What is part of a database that holds only one type of information?",
+      answers: {
+        a: "Report",
+        b: "Field",
+        c: "Record"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "'OS' computer abbreviation usually means ?",
+      answers: {
+        a: "Open Software",
+        b: "Order of Significance",
+        c: "Opertating System"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "In which decade with the first transatlantic radio broadcast occur?",
+      answers: {
+        a: "1900s",
+        b: "1870s",
+        c: "1850s",
+        d: "1860s"
+      },
+      correctAnswer: "a"
+    },
+    {
+        question: "'.MOV' extension refers usually to what kind of file?",
+        answers: {
+          a: "Image file",
+          b: "Animation/movie file",
+          c: "Audio file",
+          d: "MS Office document"
+        },
+        correctAnswer: "b"
+      }
+  ];
+
+ 
+  buildQuiz();
+
+ 
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
+
